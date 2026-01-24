93af2707d57e3ae3d03126f21bd66e59f815adc2
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-01-23 22:59:54 -0500

    Update test_extractors.py

--- tests/test_extractors.py
+++ tests/test_extractors.py
@@ -3,3 +3,9 @@ from manifestfuse.extractors import extract_refs_from_text
 
 
 
+class TestExtractors(unittest.TestCase):
+    def test_liquid_asset_url(self):
+        t = """{{ 'main.css' | asset_url | stylesheet_tag }}"""
+        refs = extract_refs_from_text(t)
+        self.assertIn("main.css", refs)
+
